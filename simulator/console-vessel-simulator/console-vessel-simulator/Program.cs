1250455a3424b7f9584c08559ea82d2ce8ce3e4b
Dag König <dagk@microsoft.com>
2018-05-14 19:43:38 +0200

    first version tested

--- simulator/console-vessel-simulator/console-vessel-simulator/Program.cs
+++ simulator/console-vessel-simulator/console-vessel-simulator/Program.cs
@@ -1,12 +1,73 @@
-﻿using System;
+﻿using Microsoft.Azure.Devices.Client;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace console_vessel_simulator
 {
     class Program
     {
+        private IConfigurationRoot Configuration { get; set; }
+        private DeviceClient deviceClient;
+
         static void Main(string[] args)
         {
-            
+            new Program().Simulate();
+        }
+
+        void Simulate()
+        {
+
+            var builder = new ConfigurationBuilder()
+                .AddYamlFile("appsettings.yml", optional: false);
+
+            Configuration = builder.Build();
+
+            var connectionString = Configuration["iotedgedeviceconnectionstring"];
+
+            Console.WriteLine("Simulated device. Ctrl-C to exit.\n");
+
+            // Connect to the IoT hub using the MQTT protocol
+            deviceClient = DeviceClient.CreateFromConnectionString(connectionString, TransportType.Mqtt);
+            SendDeviceToCloudMessagesAsync();
+            Console.ReadLine();
+
+
+        }
+
+        private async void SendDeviceToCloudMessagesAsync()
+        {
+            // Initial telemetry values
+            double minTemperature = 20;
+            double minHumidity = 60;
+            Random rand = new Random();
+
+            while (true)
+            {
+                double currentTemperature = minTemperature + rand.NextDouble() * 15;
+                double currentHumidity = minHumidity + rand.NextDouble() * 20;
+
+                // Create JSON message
+                var telemetryDataPoint = new
+                {
+                    temperature = currentTemperature,
+                    humidity = currentHumidity
+                };
+                var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
+                var message = new Message(Encoding.ASCII.GetBytes(messageString));
+
+                // Add a custom application property to the message.
+                // An IoT hub can filter on these properties without access to the message body.
+                message.Properties.Add("temperatureAlert", (currentTemperature > 30) ? "true" : "false");
+
+                // Send the tlemetry message
+                await deviceClient.SendEventAsync(message);
+                Console.WriteLine("{0} > Sending message: {1}", DateTime.Now, messageString);
+
+                await Task.Delay(1000);
+            }
         }
     }
 }
