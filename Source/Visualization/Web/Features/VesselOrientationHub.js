77adf14130cc1d12a994c1462a37ba9c4416e2e3
Einar Ingebrigtsen <einar@dolittle.com>
2018-05-28 12:14:33 +0200

    Moving variables into the vessel orientation hub

--- Source/Visualization/Web/Features/VesselOrientationHub.js
+++ Source/Visualization/Web/Features/VesselOrientationHub.js
@@ -1,11 +1,20 @@
 import { HubConnection } from './HubConnection';
-
+import { observable } from 'aurelia-framework';
 
 export class VesselOrientationHub
 {
+    @observable gravityX = 0;
+    @observable gravityY = 0;
+    @observable gravityZ = 0;
+
     constructor() {
         this._connection = HubConnection.createFor('vessel/orientation');
-        //this._connection.on('gravityChanged', this.gravityChanged);
+        this._connection.on('gravityChanged', this.gravityChanged, this);
     }
 
+    gravityChanged(x,y,z) {
+        this.gravityX = x;
+        this.gravityY = y;
+        this.gravityZ = z;
+    }        
 }
--- Source/Visualization/Web/Features/index.js
+++ Source/Visualization/Web/Features/index.js
@@ -1,10 +1,8 @@
 import { inject } from 'aurelia-framework';
 import { VesselOrientationHub } from "./VesselOrientationHub";
 import { observable } from 'aurelia-framework';
-import {BindingSignaler} from 'aurelia-templating-resources';
 
-
-@inject(VesselOrientationHub, BindingSignaler)
+@inject(VesselOrientationHub)
 export class index {
 
     // Speed on water
@@ -18,27 +16,7 @@ export class index {
 
     // Friction
 
-
-    @observable gravityX = 42;
-    @observable gravityY = 43;
-    @observable gravityZ = 44;
-
-    constructor(vesselOrientationHub, signaler) {
-        this.vesselOrientationHub = vesselOrientationHub;
+    constructor(vesselOrientationHub) {
+        this.orientation = vesselOrientationHub;
     }
-
-    attached() {
-        this.vesselOrientationHub._connection.on('gravityChanged', this.gravityChanged, this);
-
-        setTimeout(() => this.gravityX = 43, 1000);
-    }
-
-    gravityChanged(x,y,z) {
-        this.gravityX = x;
-        this.gravityY = y;
-        this.gravityZ = z;
-
-        console.log(`Gravity changed ${x},${y},${z}`);
-        //setTimeout(() => this.gravityX = 43, 1000);
-    }    
 }
