b4e0a70200b553252e9a69d1c6849b4e4777eb46
Vidar Aune Westrum <vidar@dolittle.com>
2018-05-28 14:48:31 +0200

    Initializing grid view

--- Source/Visualization/Web/Features/VesselOrientationHub.js
+++ Source/Visualization/Web/Features/VesselOrientationHub.js
@@ -1,8 +1,7 @@
 import { HubConnection } from './HubConnection';
 import { observable } from 'aurelia-framework';
 
-export class VesselOrientationHub
-{
+export class VesselOrientationHub {
     @observable gravityX = 0;
     @observable gravityY = 0;
     @observable gravityZ = 0;
@@ -12,9 +11,9 @@ export class VesselOrientationHub
         this._connection.on('gravityChanged', this.gravityChanged, this);
     }
 
-    gravityChanged(x,y,z) {
+    gravityChanged(x, y, z) {
         this.gravityX = x;
         this.gravityY = y;
         this.gravityZ = z;
-    }        
+    }
 }
