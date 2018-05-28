32403171c269d74deb31fc347e087c716f8de819
Einar Ingebrigtsen <einar@dolittle.com>
2018-05-28 12:05:17 +0200

    The rest

---
+++ Source/Visualization/Read/Vessel/OrientationProcessors.cs
@@ -0,0 +1,15 @@
+using System;
+using Dolittle.Events.Processing;
+using ExternalEvents;
+
+namespace Read.Vessel
+{
+    public class OrientationProcessors : ICanProcessEvents
+    {
+
+        public void Process(GravityChanged @event)
+        {
+            
+        }
+    }
+}
