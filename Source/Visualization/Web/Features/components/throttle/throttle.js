9b9b697b5ea51a5597e53e85da240029469a90a0
Vidar Aune Westrum <vidar@dolittle.com>
2018-05-30 16:02:16 +0200

    connected the two engines

--- Source/Visualization/Web/Features/components/throttle/throttle.js
+++ Source/Visualization/Web/Features/components/throttle/throttle.js
@@ -5,15 +5,17 @@ import { ChangeThrottle } from './ChangeThrottle';
 @containerless()
 @inject(CommandCoordinator)
 export class throttle {
-  @observable engine_a_throttle;
-  @observable engine_b_throttle;
+  @observable engine_a_throttle = 0;
+  @observable engine_b_throttle = 0;
+  @observable engines_connected = true;
   constructor(command_coordinator) {
     this._command_coordinator = command_coordinator;
-    this.engine_a_throttle = 50;
-    this.engine_b_throttle = 50;
   }
 
   engine_a_throttleChanged(value) {
+    if (this.engines_connected && this.engine_b_throttle !== value) {
+      this.engine_b_throttle = value;
+    }
     let command = new ChangeThrottle();
     command.engine = 0;
     command.target = value;
@@ -21,6 +23,9 @@ export class throttle {
   }
 
   engine_b_throttleChanged(value) {
+    if (this.engines_connected && this.engine_a_throttle !== value) {
+      this.engine_a_throttle = value;
+    }
     let command = new ChangeThrottle();
     command.engine = 1;
     command.target = value;
