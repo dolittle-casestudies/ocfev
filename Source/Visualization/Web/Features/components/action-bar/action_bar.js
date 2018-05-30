ccc58e076ed639fe31cd32989331746db1c7585e
Vidar Aune Westrum <vidar@dolittle.com>
2018-05-30 14:55:35 +0200

    moved the throttle sliders to the throttle

--- Source/Visualization/Web/Features/components/action-bar/action_bar.js
+++ Source/Visualization/Web/Features/components/action-bar/action_bar.js
@@ -1,35 +1,7 @@
-import { containerless, customElement, bindable, observable } from 'aurelia-framework';
-import { CommandCoordinator } from '@dolittle/commands';
-import { inject } from 'aurelia-dependency-injection';
-
-import { ChangeThrottle } from './ChangeThrottle';
-
+import { containerless, customElement } from 'aurelia-framework';
 
 @customElement('action-bar')
 @containerless()
-@inject(CommandCoordinator)
 export class action_bar {
-  @bindable to;
-  @observable engine_a_throttle;
-  @observable engine_b_throttle;
-
-  constructor(command_coordinator) {
-    this._command_coordinator = command_coordinator;
-    this.engine_a_throttle = 0;
-    this.engine_b_throttle = 0;
-  }
-
-  engine_a_throttleChanged(value) {
-    let command = new ChangeThrottle();
-    command.engine = 0;
-    command.target = value;
-    this._command_coordinator.handle(command);
-  }
-
-  engine_b_throttleChanged(value) {
-    let command = new ChangeThrottle();
-    command.engine = 1;
-    command.target = value;
-    this._command_coordinator.handle(command);
-  }
+  constructor() {}
 }
--- Source/Visualization/Web/Features/components/throttle/throttle.js
+++ Source/Visualization/Web/Features/components/throttle/throttle.js
@@ -1,4 +1,29 @@
-import { containerless, bindable } from 'aurelia-framework';
+import { containerless, observable, inject } from 'aurelia-framework';
+import { CommandCoordinator } from '@dolittle/commands';
+import { ChangeThrottle } from './ChangeThrottle';
 
 @containerless()
-export class throttle {}
+@inject(CommandCoordinator)
+export class throttle {
+  @observable engine_a_throttle;
+  @observable engine_b_throttle;
+  constructor(command_coordinator) {
+    this._command_coordinator = command_coordinator;
+    this.engine_a_throttle = 50;
+    this.engine_b_throttle = 50;
+  }
+
+  engine_a_throttleChanged(value) {
+    let command = new ChangeThrottle();
+    command.engine = 0;
+    command.target = value;
+    this._command_coordinator.handle(command);
+  }
+
+  engine_b_throttleChanged(value) {
+    let command = new ChangeThrottle();
+    command.engine = 1;
+    command.target = value;
+    this._command_coordinator.handle(command);
+  }
+}
