aa2cd9517c29341d13567b8e9af17463d5c259f1
Einar Ingebrigtsen <einar@dolittle.com>
2018-05-30 06:46:06 +0200

    Reorganize

--- Source/Modules/Adapter/Ingestion/Telemetry.cs
+++ Source/Modules/Adapter/Ingestion/Telemetry.cs
@@ -3,12 +3,14 @@ namespace Ingestion
     public class Telemetry
     {
         public float angle_wind_relative { get; set; } // degrees
-        public float depth               { get; set; } // metres
-        public float list                { get; set; } // degrees
-        public float power               { get; set; } // Megawatt
-        public float sog                 { get; set; } // Knots
         public float relative_windspeed  { get; set; } // meters/second
-        public float stw                 { get; set; } // Knots
+        public float depth               { get; set; } // metres
+
+        public float power               { get; set; } // Megawatt
+        public float sog                 { get; set; } // Speed over ground, Knots
+        public float stw                 { get; set; } // Speed through water, Knots
+
+        public float list                { get; set; } // degrees
         public float trim                { get; set; } // metres
     }
 }
