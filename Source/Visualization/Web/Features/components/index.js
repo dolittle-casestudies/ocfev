49155ecfb3750a33d037806006a1bd562f55de25
Vidar Aune Westrum <vidar@dolittle.com>
2018-06-12 16:16:35 +0200

    added ship and renamed gravity to trim

--- Source/Visualization/Web/Features/components/index.js
+++ Source/Visualization/Web/Features/components/index.js
@@ -5,9 +5,9 @@ export function configure(config) {
   config.globalResources(PLATFORM.moduleName('./app_header/app_header'));
   config.globalResources(PLATFORM.moduleName('./app_footer/app_footer'));
   config.globalResources(PLATFORM.moduleName('./chart/chart'));
-  config.globalResources(PLATFORM.moduleName('./gravity/gravity'));
   config.globalResources(PLATFORM.moduleName('./map/map'));
   config.globalResources(PLATFORM.moduleName('./speed/speed'));
   config.globalResources(PLATFORM.moduleName('./throttle/throttle'));
+  config.globalResources(PLATFORM.moduleName('./trim/trim'));
   config.globalResources(PLATFORM.moduleName('./wind/wind'));
 }
