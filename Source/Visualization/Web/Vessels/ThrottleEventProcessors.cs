4f642d33db30f1eff3b7b6a7eb43314a3f2ba301
Einar Ingebrigtsen <einar@dolittle.com>
2018-06-13 15:40:37 +0200

    Introducing direct control of engines

---
+++ Source/Visualization/Web/Vessels/ThrottleEventProcessors.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Net;
+using Dolittle.Events.Processing;
+using Dolittle.Logging;
+using Events.Vessels;
+
+namespace Web.Vessels
+{
+    public class ThrottleEventProcessors : ICanProcessEvents
+    {
+        readonly ILogger _logger;
+
+        public ThrottleEventProcessors(ILogger logger)
+        {
+            _logger = logger;
+        }
+        public void Process(ThrottleChanged @event)
+        {
+            _logger.Information($"Throttle Changed : {@event.Engine} - {@event.Target}");
+            var actualThrottle = @event.Target / 100f;
+            var request = (HttpWebRequest)WebRequest.Create($"http://192.168.10.204:8080?engine={@event.Engine}&throttle={actualThrottle}");
+            request.GetResponse();
+        }
+    }
+}
