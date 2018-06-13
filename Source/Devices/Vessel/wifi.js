093b466cbf295186be7440ccc426951dd79403f2
Einar Ingebrigtsen <einar@dolittle.com>
2018-06-13 15:22:14 +0200

    OCFEV Wifi

--- Source/Devices/Vessel/wifi.js
+++ Source/Devices/Vessel/wifi.js
@@ -1,10 +1,12 @@
 var wifi = require("Wifi");
 wifi.scan(function(ap_list) {
   ap_list.forEach(function(ap) {
-    //console.log(ap.ssid +" - "+ap.authMode+" - "+ap.rssi);
+    console.log(ap.ssid +" - "+ap.authMode+" - "+ap.rssi);
   });
 
-  wifi.connect("The Ingebrigtsens", {password:"Agei1208"}, function(ap) {
-    console.log("Connected : "+ap);
-  });  
 });
+
+
+wifi.connect("OCFEV", {password:""}, function(ap) {
+  console.log("Connected : "+ap);
+});  
