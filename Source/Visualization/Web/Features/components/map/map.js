1ef11a546b0b146562e431bfbd6c8028ac3143d8
Vidar Aune Westrum <vidar@dolittle.com>
2018-06-14 14:47:24 +0200

    added vessel controls to map

--- Source/Visualization/Web/Features/components/map/map.js
+++ Source/Visualization/Web/Features/components/map/map.js
@@ -1,13 +1,15 @@
 import { containerless, observable, inject } from 'aurelia-framework';
 import L from 'leaflet';
-import lr from 'leaflet-rotatedmarker';
 import map_marker from './map_marker_icon';
 import { HttpClient } from 'aurelia-http-client';
+import 'leaflet-rotatedmarker';
+import vessel_icon from './vessel_control_icon.svg';
 
 @containerless()
 @inject(HttpClient)
 export class map {
-    @observable vessel_marker_rotation = 23;
+    @observable vessel_marker_rotation = 0;
+
     constructor(httpClient) {
         this.vessel_marker;
         this.lat_long_center_init = { lat: 59.11927, lng: 10.223576 };
@@ -20,24 +22,25 @@ export class map {
             this.map = this._createMap();
             if (this.vessel_marker_location) {
                 this._add_vessel_marker(this.vessel_marker_location);
-                //replace map
+                this.map.setView(this.vessel_marker_location, 10);
             }
         }
+        this.vessel_marker_rotation = parseInt(this._get_vessel_rotation_from_local_storage(), 10);
     }
 
     vessel_marker_rotationChanged(nv, ov) {
-        console.log(nv, ov);
+        this._update_vessel_rotation_storage();
         this._update_vessel_marker(this.vessel_marker_location);
     }
 
     _createMap() {
-        let _self = this;
+        let self = this;
         let _map = new L.Map(this.map_container, {
             center: [this.lat_long_center_init.lat, this.lat_long_center_init.lng],
-            zoom: 12
-        }).on('click', function (e) {
+            zoom: 10
+        }).on('click', function(e) {
             let _latlng = { lat: e.latlng.lat, lng: e.latlng.lng };
-            _self._update_vessel_marker(_latlng);
+            self._update_vessel_marker(_latlng);
         });
 
         L.tileLayer('https://{s}.tile.openstreetmap.se/hydda/base/{z}/{x}/{y}.png', {
@@ -45,8 +48,72 @@ export class map {
                 'Tiles courtesy of <a href="http://openstreetmap.se/" target="_blank">OpenStreetMap Sweden</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         }).addTo(_map);
 
+        this._create_vessel_control(self);
+        L.control.vessel({ position: 'topright' }).addTo(_map);
+
         return _map;
     }
+
+    _create_vessel_control(self) {
+        L.Control.Vessel = L.Control.extend({
+            onAdd: function() {
+                let wrapper = L.DomUtil.create('div');
+                L.DomUtil.addClass(wrapper, 'vessel_control');
+                L.DomUtil.addClass(wrapper, 'leaflet-bar');
+
+                let center_button = L.DomUtil.create('a', 'center_map', wrapper);
+                L.DomUtil.addClass(center_button, 'center_map_to_vessel');
+                center_button.title = 'Center map to vessel location';
+                center_button.setAttribute('role', 'button');
+                let icon = L.DomUtil.create('img', 'vessel_icon', center_button);
+                icon.src = vessel_icon;
+                // center_button.innerHTML = '&#9737;';
+
+                let rotate_cw = L.DomUtil.create('a', 'rotate_cw', wrapper);
+                rotate_cw.title = 'rotate the vessel clockwise';
+                rotate_cw.setAttribute('role', 'button');
+                rotate_cw.innerHTML = '&cudarrl;';
+
+                let rotate_ccw = L.DomUtil.create('a', 'rotate_ccw', wrapper);
+                rotate_ccw.title = 'rotate the vessel counter clockwise';
+                rotate_ccw.setAttribute('role', 'button');
+                rotate_ccw.innerHTML = '&cudarrl;';
+
+                L.DomEvent.disableClickPropagation(center_button);
+                L.DomEvent.disableClickPropagation(rotate_cw);
+                L.DomEvent.disableClickPropagation(rotate_ccw);
+                L.DomEvent.on(center_button, 'click ', this._center_map_to_vessel);
+                L.DomEvent.on(rotate_cw, 'click', this._rotate_vessel_cw);
+                L.DomEvent.on(rotate_ccw, 'click', this._rotate_vessel_ccw);
+
+                return wrapper;
+            },
+            onRemove: function() {
+                L.DomEvent.off(center_button, 'click ', this._center_map_to_vessel);
+                L.DomEvent.off(rotate_cw, 'click', this._rotate_vessel_cw);
+                L.DomEvent.off(rotate_ccw, 'click', this._rotate_vessel_ccw);
+            },
+            _center_map_to_vessel: function(ev) {
+                if (!self.vessel_marker_location) {
+                    let _latlng = { lat: 59.90700614346053, lng: 10.75143098831177 };
+                    self._add_vessel_marker(_latlng);
+                    self.vessel_marker_rotation = -60;
+                }
+                self.map.setView(self.vessel_marker_location, 15);
+            },
+            _rotate_vessel_cw: function(ev) {
+                self.vessel_marker_rotation += 5;
+            },
+            _rotate_vessel_ccw: function(ev) {
+                self.vessel_marker_rotation -= 5;
+            }
+        });
+
+        L.control.vessel = function(opts) {
+            return new L.Control.Vessel(opts);
+        };
+    }
+
     _add_vessel_marker(latlng) {
         let _map_marker = new map_marker();
         this.vessel_marker = this._create_vessel_marker(latlng, _map_marker);
@@ -65,21 +132,34 @@ export class map {
     _update_vessel_marker_location() {
         this.vessel_marker_location = this._get_vessel_location_from_local_storage();
     }
+
     _create_vessel_marker(latlng, markerIcon) {
-        let _self = this;
+        let self = this;
         let _marker = L.marker([latlng.lat, latlng.lng], {
             icon: markerIcon,
             riseOnHover: true,
             draggable: true,
             autoPan: true,
-            rotationAngle: _self.vessel_marker_rotation
+            rotationAngle: self.vessel_marker_rotation
         });
-        _marker.on('dragend', function (e) {
-            var latlng = _marker.getLatLng();
-            _self._update_vessel_location_storage(latlng);
-            _self._update_vessel_location(latlng);
+        _marker.on('dragend', function(e) {
+            let _latlng = _marker.getLatLng();
+            self._update_vessel_location_storage(_latlng);
+            self._update_vessel_location(_latlng);
         });
         return _marker;
+    }
+
+    _update_vessel_rotation_storage() {
+        localStorage.setItem('vessel_rotation', this.vessel_marker_rotation);
+    }
+
+    _get_vessel_rotation_from_local_storage() {
+        let rotation = localStorage.getItem('vessel_rotation');
+        if (!rotation) {
+            return 0;
+        }
+        return rotation;
     }
 
     _update_vessel_location_storage(latlng) {
@@ -87,6 +167,7 @@ export class map {
         localStorage.setItem('vessel_longitude', latlng.lng);
         this._update_vessel_marker_location();
     }
+
     _get_vessel_location_from_local_storage() {
         let vessel_lat = localStorage.getItem('vessel_latitude');
         let vessel_lng = localStorage.getItem('vessel_longitude');
@@ -98,11 +179,12 @@ export class map {
     }
 
     _update_vessel_location(latlng) {
-        this._httpClient.createRequest(`/api/location?latitude=${latlng.lat}&longitude=${latlng.lng}`)
+        this._httpClient
+            .createRequest(`/api/location?latitude=${latlng.lat}&longitude=${latlng.lng}`)
             .asGet()
             .send()
             .then(result => {
-                console.log("Location sent");
-            });        
+                console.log('Location sent');
+            });
     }
 }
--- Source/Visualization/Web/Features/components/map/map_marker_icon.js
+++ Source/Visualization/Web/Features/components/map/map_marker_icon.js
@@ -2,24 +2,24 @@ import vessel_marker from './vessel.png';
 import L from 'leaflet';
 
 class map_marker {
-  constructor() {
-    this.iconSize = [18, 61];
-    this.iconAnchor = [9, 30];
-    this.popupAnchor = [0, -61];
-    this.iconUrl = this._get_icon_file();
-  }
+    constructor() {
+        this.iconSize = [18, 61];
+        this.iconAnchor = [9, 30];
+        this.popupAnchor = [0, -61];
+        this.iconUrl = this._get_icon_file();
+    }
 
-  _get_icon_file(category) {
-    return vessel_marker;
-  }
+    _get_icon_file(category) {
+        return vessel_marker;
+    }
 }
 
 export default function() {
-  let _map_marker = new map_marker();
-  return L.icon({
-    iconUrl: _map_marker.iconUrl,
-    iconSize: _map_marker.iconSize,
-    iconAnchor: _map_marker.iconAnchor,
-    popupAnchor: _map_marker.popupAnchor
-  });
+    let _map_marker = new map_marker();
+    return L.icon({
+        iconUrl: _map_marker.iconUrl,
+        iconSize: _map_marker.iconSize,
+        iconAnchor: _map_marker.iconAnchor,
+        popupAnchor: _map_marker.popupAnchor
+    });
 }
