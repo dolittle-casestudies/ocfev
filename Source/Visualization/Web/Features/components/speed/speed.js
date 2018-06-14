075f5200c67511c8f62784a229a57465ee12d70a
Einar Ingebrigtsen <einar@dolittle.com>
2018-06-14 16:57:52 +0200

    Speed through water up and running

--- Source/Visualization/Web/Features/components/speed/speed.js
+++ Source/Visualization/Web/Features/components/speed/speed.js
@@ -1,6 +1,24 @@
-import { containerless, bindable } from 'aurelia-framework';
+import { containerless, observable, inject } from 'aurelia-framework';
+import { EventAggregator } from 'aurelia-event-aggregator';
 
 @containerless()
+@inject(EventAggregator)
 export class speed {
-  @bindable speed = '18,2';
+  @observable speed = 0;
+  @observable actual_speed = 0;
+
+  constructor(event_aggregator) {
+      event_aggregator.subscribe("changeThrottle", response => {
+          this.speed = Math.pow((response.target/100) * (20 / 0.0025), 1/3);
+      });      
+
+      setInterval(() => {
+          if( this.speed == 0 ) {
+              this.actual_speed = 0;
+          } else {
+            this.actual_speed = new Number((this.speed + (Math.random()/10))).toFixed(2);
+          }
+
+      },300);
+  }
 }
