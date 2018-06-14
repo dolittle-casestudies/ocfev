571461f854894b76f9946a60cb692db430c881cd
Einar Ingebrigtsen <einar@dolittle.com>
2018-06-14 16:54:53 +0200

    Publish to event aggregator the thrtottle

--- Source/Visualization/Web/Features/components/throttle/throttle.js
+++ Source/Visualization/Web/Features/components/throttle/throttle.js
@@ -1,15 +1,17 @@
 import { containerless, observable, inject } from 'aurelia-framework';
 import { CommandCoordinator } from '@dolittle/commands';
 import { ChangeThrottle } from './ChangeThrottle';
+import { EventAggregator } from 'aurelia-event-aggregator';
 
 @containerless()
-@inject(CommandCoordinator)
+@inject(CommandCoordinator, EventAggregator)
 export class throttle {
   @observable engine_a_throttle = 0;
   @observable engine_b_throttle = 0;
   @observable engines_connected = true;
-  constructor(command_coordinator) {
+  constructor(command_coordinator, event_aggregator) {
     this._command_coordinator = command_coordinator;
+    this._event_aggregator = event_aggregator;
   }
 
   engine_a_throttleChanged(value) {
@@ -19,6 +21,8 @@ export class throttle {
     let command = new ChangeThrottle();
     command.engine = 0;
     command.target = value;
+
+    this._event_aggregator.publish('changeThrottle', command);
     this._command_coordinator.handle(command);
   }
 
@@ -29,6 +33,7 @@ export class throttle {
     let command = new ChangeThrottle();
     command.engine = 1;
     command.target = value;
+
     this._command_coordinator.handle(command);
   }
 
