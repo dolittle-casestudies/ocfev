785a22a60ad663150e898107f216d3ffb8e0a8f4
Einar Ingebrigtsen <einar@dolittle.com>
2018-06-15 10:53:45 +0200

    Fixed title of graph

--- Source/Visualization/Web/Features/components/chart/chart.js
+++ Source/Visualization/Web/Features/components/chart/chart.js
@@ -14,7 +14,7 @@ export class chart {
         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
         datasets: [
           {
-            label: '# of Votes',
+            label: 'Engine efficiency',
             data: [12, 19, 3, 5, 2, 3],
             backgroundColor: [
               'rgba(255, 99, 132, 0.2)',
