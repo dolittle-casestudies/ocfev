baa003ec744315255d39d082f0bf4e6ff8710433
Einar Ingebrigtsen <einar@dolittle.com>
2018-10-15 15:11:55 +0200

    Removing traces of Kafka

--- Source/Visualization/Dockerize.sh
+++ Source/Visualization/Dockerize.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd ../..
-docker build --add-host "kafka-service:137.117.175.54" -t dolittle/ocfev-visualization -f Source/Visualization/Dockerfile .
+docker build -t dolittle/ocfev-visualization -f Source/Visualization/Dockerfile .
